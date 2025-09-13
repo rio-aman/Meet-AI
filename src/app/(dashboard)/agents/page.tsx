b299096f416cdefcad40cfae861fc92a918167ac
rio-aman <amprajapati08893.com>
2025-09-13 23:10:16 +0530

    09 - Add agents management features with error and loading states
    - Implement agents router with TRPC integration.
    - Create AgentsView, AgentsViewLoading, and AgentsViewError components.
    - Add ErrorState and LoadingState components for better user feedback.
    - Update package dependencies to include nanoid and react-error-boundary.
    - Refactor HomeView to remove unnecessary TRPC query.

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
+
+const Page = async () => {
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<AgentsViewLoading />}>
+        {/* when error boundary downloaded */}
+        <ErrorBoundary fallback={<AgentsViewError />}>
+            <AgentsView />
+        </ErrorBoundary>
+        </Suspense>
+    </HydrationBoundary>
+  )
+};
+
+export default Page;
---
+++ src/modules/agents/server/procedures.ts
@@ -0,0 +1,15 @@
+import { db } from "@/db";
+import { agents } from "@/db/schema";
+import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+
+export const agentsRouter = createTRPCRouter({
+    getMany: baseProcedure.query(async () => {
+        const data = await db.select().from(agents);
+
+        // await new Promise((resolve) => setTimeout(resolve, 5000));
+        // throw new TRPCError({ code: "BAD_REQUEST" });
+        // this above is for testing the app for the error and loading if data present or in error
+
+        return data;
+    }),
+}) 
