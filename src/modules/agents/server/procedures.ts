70a63e0dfcfe609c2e997c5f50a96ace9bb2cea1
rio-aman <amprajapati08893.com>
2025-09-16 21:25:17 +0530

    13 - integrated nuqs for improved agents filtering and pagination; add search filters and pagination components

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,10 @@
+import { z } from "zod";
+import { agentsInsertSchema } from "../schema";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schema";
-import { z } from "zod";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 
 export const agentsRouter = createTRPCRouter({
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({input}) => {
@@ -18,14 +19,60 @@ export const agentsRouter = createTRPCRouter({
         return existingAgent;
     }),
 
-    getMany: protectedProcedure.query(async () => {
-        const data = await db.select().from(agents);
+    getMany: protectedProcedure
+    .input(
+        z.object({
+            page: z.number().default(DEFAULT_PAGE),
+            pageSize: z
+                .number()
+                .min(MIN_PAGE_SIZE)
+                .max(MAX_PAGE_SIZE)
+                .default(DEFAULT_PAGE_SIZE),
+            search: z.string().nullish()
+        })
+    )
+    .query(async ({ ctx, input }) => {
+        const { search, page, pageSize } = input;
+
+        const data = await db.select({
+            // TODO: to change the actual count
+            ...getTableColumns(agents),
+            meetingCount: sql<number>`6`,
+        })
+        .from(agents)
+        .where(
+            and(
+                eq(agents.userId, ctx.auth.user.id),
+                search ? ilike(agents.name, `${search}%`) : undefined,
+            )
+        )
+        .orderBy(desc(agents.createdAt), desc(agents.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize)
+
+        const [total] = await db
+            .select({count: count()})
+            .from(agents)
+            .where(
+                and(
+                    eq(agents.userId, ctx.auth.user.id),
+                    search ? ilike(agents.name, `${search}%`) : undefined,
+                )
+            );
+        
+        const totalPages = Math.ceil(total.count / pageSize);
+
+        return {
+            items: data,
+            total: total.count,
+            totalPages,
+        };
 
         // await new Promise((resolve) => setTimeout(resolve, 5000));
         // throw new TRPCError({ code: "BAD_REQUEST" });
         // this above is for testing the app for the error and loading if data present or in error
 
-        return data;
+        // return data;
     }),
 
     create: protectedProcedure
