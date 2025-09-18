291261e592cb670139fc07e0963d970dc415633b
rio-aman <amprajapati08893.com>
2025-09-18 22:37:36 +0530

    17 - meeting form means (meetings) implement meeting creation and also can create agent via meeting form

---
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { NewMeetingDialog } from "./new-meeting-dialog";
+import { useState } from "react";
+
+export const MeetingsListHeader = () => {
+    const [isDialogOpened, setIsDialogOpened] = useState(false);
+
+    return (
+        <>
+            <NewMeetingDialog open={isDialogOpened} onOpenChange={setIsDialogOpened}/>
+            <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <div className="flex items-center justify-between">
+                    <h5 className="font-medium text-xl">My Meetings</h5>
+                    <Button onClick={() => setIsDialogOpened(true)}>
+                        <PlusIcon />
+                        New Meeting
+                    </Button>
+                </div>
+                <div className="flex items-center gap-x-2 p-1">
+                    TODO: Filters
+                </div>
+            </div>
+        </>
+    );
+};
